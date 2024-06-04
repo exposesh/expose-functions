be894ad8f5cfe791afd2579da17f5718689b6457
Gaëtan L. H.-F. <81755261+gaetanlhf@users.noreply.github.com>
2024-06-04 15:04:47 +0200

    Update checkIfStarred.js

--- functions/src/checkIfStarred.js
+++ functions/src/checkIfStarred.js
@@ -4,9 +4,20 @@ const admin = require("firebase-admin");
  * @param {string} username
  */
 async function checkIfStarred(username) {
-  const starsRef = admin.firestore().collectionGroup("users").where(admin.firestore.FieldPath.documentId(), "==", username);
-  const starsQuerySnapshot = await starsRef.get();
-  return !starsQuerySnapshot.empty;
+  const starsCollection = admin.firestore().collection("stars");
+  const querySnapshot = await starsCollection.listDocuments();
+  let userExists = false;
+
+  for (const docRef of querySnapshot) {
+    const usersCollection = docRef.collection("users");
+    const userDoc = await usersCollection.doc(username).get();
+    if (userDoc.exists) {
+      userExists = true;
+      break;
+    }
+  }
+
+  return userExists;
 }
 
 module.exports = {
